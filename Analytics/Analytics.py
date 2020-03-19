3d7e6ceaf10becd0758237d940495cb2c927bed8
QuantumPlumber <44450703+QuantumPlumber@users.noreply.github.com>
2020-03-19 13:27:34 -0800

    re-organized project

--- Analytics/Analytics.py
+++ Analytics/Analytics.py
@@ -62,15 +62,37 @@ def candle_avg(open, high, low):
 def candle_squares(avg, open, high, low):
     sig = ((avg - open) ** 2 + (avg - high) ** 2 + (avg - low) ** 2) / 3.
 
+
 def candle_bollinger_bands(open, high, low, average, period=20):
     pad_squares = []
     for data in [open, high, low]:
-        #print(data)
+        # print(data)
         squares = (data - average) ** 2
         pad = np.concatenate((np.ones(period - 1) * squares[0], squares))
         pad_squares.append(np.convolve(pad, np.ones(period), mode='valid') / period)
 
-    sum_pad_squares = np.sum(np.array(pad_squares), axis=0)/3.
+    sum_pad_squares = np.sum(np.array(pad_squares), axis=0) / 3.
     two_sigma = 2 * np.sqrt(sum_pad_squares)
 
     return average - two_sigma, average + two_sigma
+
+
+def derivative(data, period=20):
+    derivative = np.zeros_like(data)
+    for i in np.arange(start=period, stop=data.shape[0]):
+        derivative[i] = (data[i] - data[i - period]) / period
+
+    derivative[0:period] = np.ones_like(period - 1) * derivative[period]
+
+    return derivative
+
+
+def second_derivative(data, period=20):
+    derivative = np.zeros_like(data)
+    for i in np.arange(start=period, stop=data.shape[0]):
+        derivative[i] = (data[i] - 2 * data[i - period // 2] + data[i - period]) / period
+
+    derivative[0:period] = np.ones_like(period - 1) * derivative[period]
+
+    return derivative
+
